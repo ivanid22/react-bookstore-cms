14066d8842dbfa225347580ab66601a8dec35bec
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-12 17:42:38 -0500

    Created the combined reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import combinedReducers from './reducers/index';
+
+const initialState = [
+  {
+    title: 'Operating Systems',
+    category: 'Learning',
+    id: Math.random(),
+  },
+  {
+    title: 'The hunger games',
+    category: 'Sci-Fi',
+    id: Math.random(),
+  },
+];
+
+const store = createStore(combinedReducers, initialState);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
