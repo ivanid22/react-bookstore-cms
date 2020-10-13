ebdbf91f6e76f94d209a0ef28184d4df3909940e
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-10-13 12:00:56 -0500

    Implement CategoryFilter component

---
+++ src/components/CategoryFilter.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import filter from '../actions/index';
+
+const CategoryFilter = props => {
+  const { filterOptions, handleFilterChange } = props;
+
+  const optionList = () => (
+    filterOptions.map(option => (
+      <option key={`${option}-key`} value={option}>{option}</option>
+    ))
+  );
+
+  const handleEventFilterChange = event => {
+    handleFilterChange(event.target.value);
+  };
+
+  return (
+    <label htmlFor="filter">
+      <select id="filter" onChange={handleEventFilterChange}>
+        {optionList()}
+      </select>
+    </label>
+  );
+};
+
+CategoryFilter.propTypes = {
+  filterOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleFilterChange: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  handleFilterChange: label => {
+    dispatch(filter(label));
+  },
+});
+
+const ConnectedCategoryFilter = connect(null, mapDispatchToProps)(CategoryFilter);
+
+export default ConnectedCategoryFilter;
