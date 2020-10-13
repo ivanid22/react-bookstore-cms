b3049c11a889c76ddbe9a406a76ee6ffc7db2876
Ivan Diaz <ivanid22@gmail.com>
2020-10-13 12:44:28 -0300

    Add Button component connection to removeBook action

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { removeBook } from '../actions/index';
 
 const Book = props => {
-  const { book } = props;
+  const { book, deleteBook } = props;
+
+  const handleClick = () => {
+    deleteBook(book);
+  };
 
   return (
     <tr>
       <td>{book.id}</td>
       <td>{book.title}</td>
       <td>{book.category}</td>
+      <td>
+        <button type="button" onClick={handleClick}>
+          <span className="material-icons">
+            delete
+          </span>
+        </button>
+      </td>
     </tr>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.objectOf(PropTypes.string).isRequired,
+  deleteBook: PropTypes.func.isRequired,
 };
 
-export default Book;
+const mapDispatchToProps = dispatch => ({
+  deleteBook: book => {
+    dispatch(removeBook(book));
+  },
+});
+
+const ConnectedBook = connect(null, mapDispatchToProps)(Book);
+
+export default ConnectedBook;
